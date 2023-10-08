91bdee2f2b892820d8f393296214d416864d451d
yuto0602 <rinrin.abc1818@gmail.com>
2023-10-08 16:48:31 +0900

    レイアウトの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ApplicationRecord
     validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
     validates :last_name, presence: true, length: { maximum: 50 }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :first_name, presence: true, length: { maximum: 50 }, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :occupation_id, numericality: { other_than: 1, message: "can't be blank"} 
+    validates :occupation_id, numericality: { other_than: 1, message: "職業を選択してください。"} 
 end
