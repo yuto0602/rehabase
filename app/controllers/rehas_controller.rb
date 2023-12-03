3ef9afa65d44714a186d9c4e24bfb015c89420fe
yuto0602 <rinrin.abc1818@gmail.com>
2023-12-03 14:16:39 +0900

    投稿機能の実装

--- app/controllers/rehas_controller.rb
+++ app/controllers/rehas_controller.rb
@@ -1,2 +1,29 @@
 class RehasController < ApplicationController
+
+  def new
+    @reha = Reha.new
+  end
+
+  def create
+    @reha = Reha.new(reha_params)
+    if @reha.save
+      redirect_to @reha, notice: '記事が正常に投稿されました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reha_params
+    params.require(:reha).permit(
+      :category,
+      :disease,
+      :complication,
+      :personality,
+      :rehabilitation,
+      :evidence_id,
+      :keyword
+    )
+  end
 end
