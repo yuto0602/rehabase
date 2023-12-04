5aaec85a1c686e436c94180d2326adc8532dbb48
yuto0602 <rinrin.abc1818@gmail.com>
2023-12-04 15:35:52 +0900

    バリデーションの設定

--- app/models/reha.rb
+++ app/models/reha.rb
@@ -1,2 +1,10 @@
 class Reha < ApplicationRecord
+
+  belongs_to :user
+
+  validates :category, presence: true
+  validates :disease, presence: true
+  validates :personality, presence: true
+  validates :keyword, presence: true
+  validates :evidence_id, presence: true
 end
