4f2a64257d5ac0d11bcb8fa8b9a70348e25b43f4
yuto0602 <rinrin.abc1818@gmail.com>
2023-12-08 18:36:51 +0900

    検索フォームの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'rehas#index'
   resources :rehas, only: [:new, :create, :show, :edit, :update, :destroy]
+  # ここにルーティングを書くところから
 end
