2922b9c3fcacadb7a38dea5afebdf2ebbfd3bac1
yuto0602 <rinrin.abc1818@gmail.com>
2023-12-09 14:28:42 +0900

    検索フォームの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'rehas#index'
-  resources :rehas, only: [:new, :create, :show, :edit, :update, :destroy]
-  # ここにルーティングを書くところから
+  
+  resources :rehas, only: [:new, :create, :show, :edit, :update, :destroy] do
+    collection do
+      get 'search'
+    end
+  end
 end
+
